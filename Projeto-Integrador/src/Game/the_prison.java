ddfbf871cae8949c492b9d3a916a00718f83dd20
Ji Hye <56440666+jihye83@users.noreply.github.com>
2019-10-13 19:23:57 -0300

    Update the_prison.java
    soh para começarmos a pensar... como começar. peguei isso do professor.

--- Projeto-Integrador/src/Game/the_prison.java
+++ Projeto-Integrador/src/Game/the_prison.java
@@ -3,8 +3,37 @@ import java.util.Scanner;
 public class the_prison {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner imput = new Scanner(System.in);
 
-	}
+        System.out.println("BEM VINDO AO JOGO 'THE PRISON'.");
+        int opcao;
+        do {
+            System.out.println("1 - Instrução do Jogo: ");
+            System.out.println("2 - Começar o jogo: ");
+            System.out.println("3 - Créditos do jogo: ");
+            System.out.println("4 - Sair: ");
+            System.out.println("Escolha uma opção: ");
+            opcao = imput.nextInt();
 
+            switch (opcao) {
+                case 1:
+                    System.out.println("Escrecer as instrucoes do jogo");
+                    break;
+                case 2:
+                    System.out.println("Iniciar o jogo");
+                    break;
+                case 3:
+                    System.out.println("Colocar os dreditos do jogo");
+                    break;
+                case 4:
+                    System.out.println("Você está saindo do jogo.");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+
+            }
+
+        } while (opcao != 4);
+
+    }
 }
