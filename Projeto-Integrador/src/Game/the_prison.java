46548a9edb3c5020f01e3c983513e661a2493381
Felipe <felipediasamorimpessoa@gmail.com>
2019-10-13 21:53:53 -0300

    Adicionei a introdução e objetivo inicial do jogo.

--- Projeto-Integrador/src/Game/the_prison.java
+++ Projeto-Integrador/src/Game/the_prison.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class the_prison {
 
 	public static void main(String[] args) {
-	Scanner imput = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 
         System.out.println("BEM VINDO AO JOGO 'THE PRISON'.");
         int opcao;
@@ -13,15 +13,24 @@ public class the_prison {
             System.out.println("3 - CrÃ©ditos do jogo: ");
             System.out.println("4 - Sair: ");
             System.out.println("Escolha uma opÃ§Ã£o: ");
-            opcao = imput.nextInt();
+            opcao = input.nextInt();
 
             switch (opcao) {
                 case 1:
                     System.out.println("Escrecer as instrucoes do jogo");
                     break;
                 case 2:
-                    System.out.println("Iniciar o jogo");
-                    System.out.println();
+                	// inicio do jogo
+                    System.out.println("\r\n" +  // Introdução e objetivo inicial do jogo
+                    		"Você é um estudante da área de TI e acaba de ser preso por hackear uma base militar situada em sua cidade,\r\n"+
+                    		"como consequência foi condenado a prisão e mandado para uma prisão de segurança máxima no estado do Arizona.\r\n" + 
+                    		"Você tem algumas restrições no presídio;\r\n" + 
+                    		"-proibido de acessar qualquer equipamento eletrônico;\r\n" + 
+                    		"-você está em uma área isolada dos outros detentos no nível 1 da prisão;\r\n" + 
+                    		"-você está em uma área de segurança máxima onde está isolado, sem acesso a outros detentos e sem nenhuma regalia.\r\n" + 
+                    		"objetivo do jogo;\r\n" + 
+                    		"o seu objetivo é obter informações para acessar os outros níveis da prisão, com isso você poderá ter acesso a outras pessoas e recursos para poder sair da prisão caso não seja descoberto.\r\n" + 
+                    		"Boa sorte, detento!");
                     break;
                 case 3:
                     System.out.println("Colocar os dreditos do jogo");
