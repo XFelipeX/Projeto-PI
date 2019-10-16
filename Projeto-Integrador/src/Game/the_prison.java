2c49bc34675bf7bdb43bcd07581f0601d1c634be
felipe.dapessoa <felipe.dapessoa@CAS419388W0712.senacsp.edu.br>
2019-10-15 22:34:11 -0300

    adicionei as instruções e modifiquei a introdução do jogo.

--- Projeto-Integrador/src/Game/the_prison.java
+++ Projeto-Integrador/src/Game/the_prison.java
@@ -19,11 +19,14 @@ public class the_prison {
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Escrecer as instrucoes do jogo");
+                	System.out.println("--------INSTRUÇÕES---------");
+                    System.out.println("1 - Cada questão vale 100 pontos de respeito.");
+                    System.out.println("2 - É necessário atingir 500 pontos de respeito para concluir o jogo.");
+                    System.out.println("3 - Caso erre uma pergunta você irá perder 50 pontos. ");
                     break;
                 case 2:
                 	// inicio do jogo
-                    System.out.println("\r\n" +  // Introdução e objetivo inicial do jogo
+                	System.out.println("\r\n" +  // Introdução e objetivo inicial do jogo
                     		"Você é um estudante da área de TI e acaba de ser preso por hackear uma base militar situada em sua cidade,\r\n"+
                     		"como consequência foi condenado a prisão e mandado para uma prisão de segurança máxima no estado do Arizona.\r\n" + 
                     		"Você tem algumas restrições no presídio;\r\n" + 
@@ -31,9 +34,12 @@ public class the_prison {
                     		"-você está em uma área isolada dos outros detentos no nível 1 da prisão;\r\n" + 
                     		"-você está em uma área de segurança máxima onde está isolado, sem acesso a outros detentos e sem nenhuma regalia.\r\n" + 
                     		"objetivo do jogo;\r\n" + 
-                    		"o seu objetivo é obter informações para acessar os outros níveis da prisão, com isso você poderá ter acesso a outras pessoas e recursos para poder sair da prisão caso não seja descoberto.\r\n" + 
+                    		"o seu objetivo é obter informações para acessar os outros níveis da prisão, com isso você poderá ter acesso \r\n" +
+                    		"a outras pessoas e recursos para poder sair da prisão caso não seja descoberto.\r\n" + 
                     		"Boa sorte, detento!");
+         System.out.println("-----------------------------------------------------------------------------------------------------------------");
                     break;
+                    
                 case 3:
                     System.out.println("Colocar os créditos do jogo");
                     break;
